55fa0056b8a4bfda915ed5934a56afa822b3ab80
@reroes <reroes799@gmail.com>
2021-04-23 23:34:21 -0500

    agregando archivo iniciales

---
+++ _build/jupyter_execute/ejemplos.py
@@ -0,0 +1,9 @@
+1Welcome to your Jupyter Book
+============================
+
+This is a small sample book to give you a feel for how book content is
+structured.
+
+Check out the content pages bundled with this sample book to get started.
+
+print("Here is some code to execute")
